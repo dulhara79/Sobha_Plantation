c1d2fe434eef9335adb63d41803d2d5786060d11
Dulhara Kaushalya <128912391+dulhara79@users.noreply.github.com>
2024-08-17 18:46:51 +0530

    Dulhara test (#39)
    * Delete hello.js
    
    * Update app.js
    
    * Create salesController.js
    
    * Create Sales.js
    
    * Create sales.js
    
    * Update app.js
    
    * Create tailwind.config.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-//pass:  5nnsrO2tPt9w60ou
-//http://localhost:5000/api/crop-varieties
-
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const connectDB = require("./config/db");
 const cropVarietiesRoutes = require("./routes/cropVarieties");
 const employeeRoutes = require("./routes/employee");
+const salesRoutes = require("./routes/sales");
 const productionRoutes = require('./routes/productionRoute.js');
 
 const app = express();
@@ -18,9 +18,10 @@ connectDB();
 // Routes
 app.use("/api/crop-varieties", cropVarietiesRoutes);
 app.use("/api/employee", employeeRoutes);
+app.use("/api/sales", salesRoutes);
 app.use('/api/production', productionRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
