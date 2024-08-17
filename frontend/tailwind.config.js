dab7b83ce1184b7750fdd81d67ecde43eb925420
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-08-17 22:47:38 +0530

    delete frontend folder

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-      './src/**/*.{js,jsx,ts,tsx}', // Adjust this path according to your project structure
-  ],
-  theme: {
-      extend: {},
-  },
-  plugins: [],
-};
