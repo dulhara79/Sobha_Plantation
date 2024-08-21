b69c8708b65cf8c58024d635cdd4bb052cc24478
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-08-21 10:50:23 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  StyledEngineProvider,
+} from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./global.css";
+
+const muiTheme = createTheme();
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </BrowserRouter>
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//      <StyledEngineProvider injectFirst>
+//        <ThemeProvider theme={muiTheme}>
+//          <CssBaseline />
+//          <App />
+//        </ThemeProvider>
+//      </StyledEngineProvider>
+//    </BrowserRouter>
+// )
