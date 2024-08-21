1c47aee93e153872414914b9129d9f8c583dc97c
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-08-22 03:04:16 +0530

    Create PageError.jsx

---
+++ frontend/src/pages/PageError.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageError = () => {
+  return (
+    <div className='content-center'>
+      <h1 className='text-center text-9xl'>Error 404</h1>
+      <h3 className='text-5xl text-center'>Page Not Found</h3>
+    </div>
+  )
+}
+
+export default PageError
