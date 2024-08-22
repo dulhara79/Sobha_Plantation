db05950dc38856d388f8b9f69cd8f2096fa161ec
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-08-22 21:22:45 +0530

    Update fertilizerRoute.js

--- backend/routes/fertilizerRoute.js
+++ backend/routes/fertilizerRoute.js
@@ -2,29 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 //Insert Model
-<<<<<<< Updated upstream
-const Fertilizer = require("../Model/FertilizerModel");
+
+const Fertilizer = require("../models/fertilizerModel");
 
 //Insert Fertilizer Controller
-const FertilizerController = require("../Controllers/FertilizerControllers");
-
-router.get("/",FertilizerController.getAllFertilizers);
-router.post("/",FertilizerController.addFertilizers);
-router.get("/:id",FertilizerController.getById);
-router.put("/:id",FertilizerController.updateFertilizer);
-router.delete("/:id",FertilizerController.deleteFertilizer);
-=======
-const Fertilizer = require("../Model/fertilizerModel");
-
-//Insert Fertilizer Controller
-const fertilizerController = require("../Controllers/fertilizerControllers");
+const fertilizerController = require("../controllers/fertilizerController");
 
 router.get("/",fertilizerController.getAllFertilizers);
 router.post("/",fertilizerController.addFertilizers);
 router.get("/:id",fertilizerController.getById);
 router.put("/:id",fertilizerController.updateFertilizer);
 router.delete("/:id",fertilizerController.deleteFertilizer);
->>>>>>> Stashed changes
+
 
 //export
 module.exports = router;
