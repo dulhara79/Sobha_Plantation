b890acfed8045135e3f20ae06ac99e7c6348d101
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-08-22 08:39:45 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../index.css";
 import PropTypes from "prop-types";
 import { Link, useLocation } from "react-router-dom";
 import {
@@ -17,6 +18,11 @@ const Sidebar = ({ className = "" }) => {
   const location = useLocation();
   const [activePage, setActivePage] = useState(location.pathname);
 
+  const isActive = (path) => {
+    const currentPath = location.pathname.split('/')[1];
+    return currentPath === path.split('/')[1];
+  };
+
   const menuItems = [
     { name: "Home", icon: <HomeOutlined />, path: "/dashboard" },
     { name: "Finances", icon: <AccountBalanceOutlined />, path: "/finance/finance/dashboard" },
@@ -31,22 +37,22 @@ const Sidebar = ({ className = "" }) => {
 
   return (
     <div
-      className={`w-[306px] shadow-lg rounded-tl-none rounded-tr-31xl rounded-br-31xl rounded-bl-none bg-whitesmoke flex flex-col items-start justify-start pt-[88px] px-2 pb-[240.3px] relative gap-[39.7px] text-left text-9xl text-gray-1700 font-roboto ${className}`}
+      className={`w-[280px] shadow-lg rounded-tl-none rounded-tr-31xl rounded-br-31xl rounded-bl-none bg-whitesmoke flex flex-col items-start justify-start pt-[70px] px-2 pb-[240.3px] fixed gap-[20px] text-left text-lg text-gray-1700 font-roboto no-underline ${className}`}
     >
       {menuItems.map((item) => (
         <Link
           to={item.path}
           key={item.name}
           onClick={() => setActivePage(item.path)}
-          className={`w-[206px] flex flex-row items-center justify-start py-2 px-[21px] rounded-full box-border text-center transition-all duration-300 ${
+          className={`w-[206px] flex flex-row items-center justify-start py-2 px-[21px] rounded-full box-border text-left transition-all duration-300 no-underline ${
             activePage === item.path
               ? "bg-[#60DB19] bg-opacity-80 text-white"
               : "bg-whitesmoke text-gray-1700"
-          } hover:bg-[#a3a3a3] hover:bg-opacity-60`}
+          } hover:bg-[#a3a3a3] hover:bg-opacity-60 hover:text-2xl`}
         >
-          <div className="flex flex-row items-center justify-between flex-1 gap-5">
+          <div className="flex flex-row items-center flex-1 gap-5">
             {item.icon}
-            <span className="font-bold text-[inherit] whitespace-nowrap">
+            <span className="font-bold text-left no-underline whitespace-nowrap">
               {item.name}
             </span>
           </div>
