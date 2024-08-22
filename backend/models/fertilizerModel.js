c70d3ed2f30542bfe61071f63e30be6e40496f51
maheesha999 <maheeshaliyanage999@gmail.com>
2024-08-22 16:13:36 +0530

    update fertilizer

---
+++ backend/models/fertilizerModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const fertilizeSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+   
+    quantity:{
+        type:Number,
+        required:true,
+    },
+
+    storagelocation:{
+        type:String,
+        required:true,
+    }, 
+
+    addeddate:{
+        type:String,
+        required:true,
+    },
+
+    expiredate:{
+        type:String,
+        required:true,
+    }
+});
+
+module.exports = mongoose.model(
+    "FertilizerModel",
+    fertilizeSchema
+)
