9668bf84a5518bfcd5db9670d22052dbed8d9489
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2024-08-24 12:22:45 +0530

    Develop/dewdu (#71)
    * Update package.json
    
    * Update package.json
    
    * Update diseases.js
    
    * Update diseases.js
    
    * Update app.js
    
    * Update package.json
    
    * Update diseasesController.js
    
    * Update diseases.js
    
    * Update app.js
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Dulhara Kaushalya <128912391+dulhara79@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const connectDB = require("./config/db");
+
 const cropVarietiesRoutes = require("./routes/cropVarieties");
 const employeeRoutes = require("./routes/employee");
 const salesRoutes = require("./routes/sales");
 const productionRoutes = require('./routes/productionRoute.js');
+const diseasesRoutes = require("./routes/diseases");
 const harvestRoutes = require('./routes/harvest');
 const fertilizerRoutes = require('./routes/fertilizerRoute.js');
 
@@ -22,9 +24,9 @@ app.use("/api/crop-varieties", cropVarietiesRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/sales", salesRoutes);
 app.use('/api/production', productionRoutes);
+app.use("/api/diseases", diseasesRoutes);
 app.use('/api/harvest',harvestRoutes);
 app.use('/api/fertilizer',fertilizerRoutes);
-
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
