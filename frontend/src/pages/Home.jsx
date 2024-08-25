860f16fcf899ba1a3447af4c9f7ba32ce9f6a4b7
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-08-25 20:56:15 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,29 @@
-// Landing page of the website
 import React from "react";
+import { Carousel } from 'antd';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Landing page</h1>
-    </div>
-  );
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
 };
+const Home = () => (
+  <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
+);
+
 
 export default Home;
