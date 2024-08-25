b746aca374be3046737e99ba12cd23c7f0705f13
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-08-25 19:37:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,27 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 const cropVarietiesRoutes = require("./routes/cropVarieties");
-const employeeRoutes = require("./routes/employee");
+const employeeRoutes = require('./routes/employee');
+const salaryEmployeeRoutes = require("./routes/salaryEmployeeRoutes");
 const salesRoutes = require("./routes/sales");
 const productionRoutes = require('./routes/productionRoute.js');
 const harvestRoutes = require('./routes/harvest');
 const fertilizerRoutes = require('./routes/fertilizerRoute.js');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
-
 // Routes
+app.use("/api/salary-employees", salaryEmployeeRoutes);
 app.use("/api/crop-varieties", cropVarietiesRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/sales", salesRoutes);
@@ -26,7 +30,13 @@ app.use('/api/harvest',harvestRoutes);
 app.use('/api/fertilizer',fertilizerRoutes);
 
 
-const PORT = 5000 || process.env.PORT;
-app.listen(PORT, () => {
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+  // Start the server
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
