537bd7e4513569ef61724f031c57926731cfcd3e
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-08-25 10:59:57 +0530

    Update Edashboard.jsx

--- frontend/src/pages/employee/Edashboard.jsx
+++ frontend/src/pages/employee/Edashboard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
-import WeatherInf from "../../components/WeatherInf"
+import EmployeeDash from "../../components/EmployeeDash";
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
 import { Breadcrumb } from "antd";
+import EmployeeNavbar from "../../components/EmployeeNavbar";
 const Edashboard = () => {
   return (
     <div>
@@ -20,8 +21,8 @@ const Edashboard = () => {
         />
 
 
-        
-        <WeatherInf/>
+<EmployeeNavbar/>
+        <EmployeeDash/>
       </div>
     </div>
   );
