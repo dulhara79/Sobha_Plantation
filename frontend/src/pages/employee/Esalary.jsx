b95581b104bdb3063f310ece542edbfa99592e79
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-08-25 16:32:35 +0530

    Update Esalary.jsx

--- frontend/src/pages/employee/Esalary.jsx
+++ frontend/src/pages/employee/Esalary.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import EmployeeNavbar from "../../components/EmployeeNavbar";
 
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
 import { Breadcrumb } from "antd";
@@ -19,7 +20,7 @@ const Esalary = () => {
             },
           ]}
         />
-
+<EmployeeNavbar/>
 <EmployeeSalary /> 
         
         
