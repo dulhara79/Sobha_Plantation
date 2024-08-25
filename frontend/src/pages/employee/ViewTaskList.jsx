511389cf8dc641cfa133dffb00084582ee6666d4
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-08-25 19:35:48 +0530

    Create ViewTaskList.jsx

---
+++ frontend/src/pages/employee/ViewTaskList.jsx
@@ -0,0 +1,44 @@
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+import { Breadcrumb } from "antd";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import React from "react";
+import EmployeeNavbar from "../../components/EmployeeNavbar";
+import TaskList from "../../components/TaskList";
+import {SnackbarProvider} from "notistack";
+
+
+export default function ViewTaskList() {
+
+    const breadcrumbItems = [
+        { name: 'Employees', href: '/employees/home' },
+        { name: 'Assign Tasks', href: '/employees/tasks' },
+    ];
+
+
+    return (
+        <SnackbarProvider>
+        <div className="">
+           
+             
+      
+        <Header />
+                <Sidebar />
+                
+                        <div className={`ml-[300px]`}>
+                        <Breadcrumb
+          items={[
+            {
+              href: "",
+              title: <HomeOutlined />,
+            },
+          ]}
+        />
+                       <EmployeeNavbar/>
+                            <TaskList/>
+                        </div>
+                    </div>
+
+                    </SnackbarProvider>            
+    );
+};
