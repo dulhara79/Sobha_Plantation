184496d887ff1f712db475267bc313cc0bc3b6c4
PiyushiRan <piyushiranasinghe@gmail.com>
2024-08-28 12:02:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,13 +16,13 @@ import FinanceDashboard from "./pages/SalesAndFinance/Finance/FinanceDashboard.j
 // inventory
 
 // employee
-import Edashboard from "./pages/employee/Edashboard.jsx";
-import Eregistration from "./pages/employee/Eregistration.jsx";
-import Esalary from "./pages/employee/Esalary";
-import EaddTask from "./pages/employee/EaddTask.jsx";
-import EditTaskPage from "./pages/employee/EditTaskPage.jsx";
-import ViewTaskList from "./pages/employee/ViewTaskList.jsx";
-import GetAttendance from "./pages/employee/GetAttendance.jsx";
+// import Edashboard from "./pages/employee/Edashboard.jsx";
+// import Eregistration from "./pages/employee/Eregistration.jsx";
+// import Esalary from "./pages/employee/Esalary";
+// import EaddTask from "./pages/employee/EaddTask.jsx";
+// //import EditTaskPage from "./pages/employee/EditTaskPage.jsx";
+// import ViewTaskList from "./pages/employee/ViewTaskList.jsx";
+// import GetAttendance from "./pages/employee/GetAttendance.jsx";
 
 // harvest
 import HarvestDashboard from "./pages/Harvest/HarvestDashboard";
@@ -34,6 +34,7 @@ import ProductsDashboard from "./pages/Products/ProductsDashboard.jsx";
 import ProductionScheduleOverview from "./pages/Products/ProductionScheduleOverview.jsx";
 import QualityControl from "./pages/Products/QualityControl.jsx";
 import AddSchedule from "./pages/Products/AddSchedule.jsx";
+
 
 // field view
 import CultivationDashboard from "./pages/CropVarieties/CultivationDashboard.jsx";
@@ -69,13 +70,13 @@ export default function App() {
       {/* inventory */}
 
       {/* employee */}
-      <Route path="/employee/dashboard" element={<Edashboard/>}/>
+      {/* <Route path="/employee/dashboard" element={<Edashboard/>}/>
       <Route path="/employee/registration" element= {<Eregistration/>}/>
       <Route path="/employee/salary" element= {<Esalary/>}/>
       <Route path="/employee/task" element= {<EaddTask/>}/>
       <Route path="/employee/taskedit" element= {<EditTaskPage/>}/>
       <Route path="/employee/TaskListview" element= {<ViewTaskList/>}/>
-      <Route path="/employee/attendance" element= {<GetAttendance/>}/>
+      <Route path="/employee/attendance" element= {<GetAttendance/>}/> */}
 
       {/* harvest */}
       <Route path="/harvest/harvestdashboard" element={<HarvestDashboard />} />
@@ -87,15 +88,16 @@ export default function App() {
       <Route path="/products/production-overview" element={<ProductionScheduleOverview />} />
       <Route path="/products/quality-control" element={<QualityControl />} />
       <Route path="/products/addschedule" element={<AddSchedule />} />
+      
 
       {/* field view  */}
       <Route path="/cultivationDashboard" element={<CultivationDashboard />} />
       <Route path="/varietyCrop" element={<VarietyCrop />} />
-      <Route path="cvForm" element={<CropVarietyForm />} />
-      <Route path="seedlingDistribution" element={<Seedling />} />
-      <Route path="schedules" element={<Schedules />} />
-      <Route path="scheduleForm" element={<ScheduleForm />} />
-      <Route path="landPreparation" element={<LandPreparation />} />
+      <Route path="/cvForm" element={<CropVarietyForm />} />
+      <Route path="/seedlingDistribution" element={<Seedling />} />
+      <Route path="/schedules" element={<Schedules />} />
+      <Route path="/scheduleForm" element={<ScheduleForm />} />
+      <Route path="/landPreparation" element={<LandPreparation />} />
         
       {/* buyers */}
 
