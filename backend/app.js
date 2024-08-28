d7056e948578a8c600b127cacde6b4f7bc6f701f
maheesha999 <maheeshaliyanage999@gmail.com>
2024-08-28 21:11:16 +0530

    updatee inventory backend

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
-const connectDB = require("./config/db");
-const cropVarietiesRoutes = require("./routes/cropVarieties");
-const employeeRoutes = require("./routes/employee");
-const salesRoutes = require("./routes/sales");
-const productionRoutes = require('./routes/productionRoute.js');
-const harvestRoutes = require('./routes/harvest');
-const fertilizerRoutes = require('./routes/fertilizerRoute.js');
-=======
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const cors = require("cors");
@@ -19,12 +7,9 @@ const connectDB = require('./config/db'); // Import MongoDB connection function
 const employeeRoutes = require('./routes/employee');
 // const salesRoutes = require('./routes/sales');
 const productionRoutes = require('./routes/Products/productionRoute.js');
-//inventory
-const fertilizerRoutes = require('./routes/Inventory/fertilizerRoute');
-const maintenanceRoutes = require('./routes/Inventory/maintenanceRoute');
-
 const yieldRoutes = require('./routes/Harvest/yield');
 const harvestRoutes = require('./routes/Harvest/harvest');
+const qualityControlRoute = require('./routes/Products/qualityControlRoute.js');
 
 // crop
 const cropVarietiesRoutes = require('./routes/cropVarieties');
@@ -33,43 +18,75 @@ const scheduleRoutes = require("./routes/scheduleRoutes");
 const soilTestingRoutes = require("./routes/soilTestingRoutes");
 const plantGrowthRoutes = require("./routes/plantGrowthRoutes");
 
-// const salesAndFinanceRoutes = require('./routes/SalesAndFinance/Routes.js');
+//inventory
+const fertilizerRoutes = require('./routes/Inventory/fertilizerRoute.js');
+const maintenanceRoutes = require('./routes/Inventory/maintenanceRoute.js');
+/**
+ * buyer
+ */
+
+const BuyerRoutes = require('./routes/buyerRoute');
+
 const FinancialTransactionRoutes = require('./routes/SalesAndFinance/financialTransactionRoutes.js');
 const InvoiceRoutes = require('./routes/SalesAndFinance/InvoiceRoutes.js');
 const SalesAnalyticsRoutes = require('./routes/SalesAndFinance/SalesAnalyticsRoutes.js');
 const SalesTrackingRoutes = require('./routes/SalesAndFinance/SalesTrackingRoutes.js');
-const salaryEmployeeRoutes = require("./routes/salaryEmployeeRoutes");
 
+const salaryEmployeeRoutes = require("./routes/salaryEmployeeRoutes");
 const ETaskRoutes = require('./routes/ETaskRoutes');
 const diseasesRoutes = require("./routes/diseases");
->>>>>>> Stashed changes
 
 const app = express();
 
-// Middleware
+// Middleware to parse JSON request bodies
 app.use(express.json());
+app.use(cors());
 
 // Connect to MongoDB
 connectDB();
 
-// Routes
+// Define routes
+
+app.use('/api/employee', employeeRoutes);
+app.use("/api/salary-employees", salaryEmployeeRoutes);
 app.use("/api/crop-varieties", cropVarietiesRoutes);
-app.use("/api/employee", employeeRoutes);
-app.use("/api/sales", salesRoutes);
+// app.use("/api/employee", employeeRoutes);
+app.use('/api/taskRecords', ETaskRoutes);
 app.use('/api/production', productionRoutes);
-<<<<<<< Updated upstream
-app.use('/api/harvest',harvestRoutes);
-app.use('/api/fertilizer',fertilizerRoutes);
-=======
 app.use('/api/harvest', harvestRoutes);
+app.use('/api/yield', yieldRoutes);
+app.use('/api/quality-control', qualityControlRoute);
+
+//inventory
 app.use('/api/fertilizers', fertilizerRoutes);
 app.use('/api/maintenance', maintenanceRoutes);
+/**
+* crop
+*/
 
-app.use('/api/yield', yieldRoutes);
->>>>>>> Stashed changes
+app.use('/api/crop-varieties', cropVarietiesRoutes);
+app.use("/api/seedlings", seedlingRoutes);
+app.use("/api/schedules", scheduleRoutes);
+app.use("/api/soil-tests", soilTestingRoutes);
+app.use("/api/plant-growth", plantGrowthRoutes);
 
+/**
+ * Sales and Finance Routes
+ */
 
-const PORT = 5000 || process.env.PORT;
+app.use("/api/salesAndFinance/finance/transaction", FinancialTransactionRoutes);
+app.use("/api/salesAndFinance/finance/invoice", InvoiceRoutes);
+app.use("/api/salesAndFinance/sales/analytics", SalesAnalyticsRoutes);
+app.use("/api/salesAndFinance/sales/tracking", SalesTrackingRoutes);
+
+/**
+ * buyer
+ */
+
+app.use('/api/buyer', BuyerRoutes);
+
+const PORT = process.env.PORT || 8090;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,42 +5,74 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 
+/**
+ * sales and finance
+*/ 
+//sales
+import SalesAndFinanceDashboard from "./pages/SalesAndFinance/SalesAndFinanceDashboard.jsx";
+import SalesDashboard from "./pages/SalesAndFinance/Sales/SalesDashboard.jsx";
+import AddSalesRecordDashboard from "./pages/SalesAndFinance/Sales/AddSalesRecordDashboard.jsx";
+import ViewSalesRecordDashboard from "./pages/SalesAndFinance/Sales/ViewSalesRecordDashboard.jsx";
+import SalesAnalyticsPage from "./pages/SalesAndFinance/Sales/SalesAnalyticsPage.jsx";
 // finance
+import FinanceDashboard from "./pages/SalesAndFinance/Finance/FinanceDashboard.jsx";
+import AddTransactionPage from "./pages/SalesAndFinance/Finance/AddTransactionPage.jsx";
+import TransactionDisplay from "./pages/SalesAndFinance/Finance/TransactionDisplay.jsx";
+import FinancialAnalyticsPage from "./pages/SalesAndFinance/Finance/FinancialAnalyticsPage.jsx";
 
 // inventory
-import InventoryDashboard from "./pages/Inventory/InventoryDashboard.jsx";
-import Fertilizer from"./pages/Inventory/Fertilizer.jsx";
-import FertilizerForm from "./pages/Inventory/FertilizerForm.jsx";
-import Maintenance from "./pages/Inventory/Maintenance.jsx";
-import MaintenanceForm from "./pages/Inventory/MaintenanceForm.jsx";
-
+ import Fertilizer from "./pages/Inventory/Fertilizer.jsx";
+ import FertilizerForm from "./pages/Inventory/FertilizerForm.jsx";
+ import InventoryDashboard from "./pages/Inventory/InventoryDashboard.jsx";
+ import Maintenance from "./pages/Inventory/Maintenance.jsx";
+ import MaintenanceForm from "./pages/Inventory/MaintenanceForm.jsx";
 // employee
+import Edashboard from "./pages/employee/Edashboard.jsx";
+import Eregistration from "./pages/employee/Eregistration.jsx";
+import Esalary from "./pages/employee/Esalary";
+import EaddTask from "./pages/employee/EaddTask.jsx";
+import EditTaskPage from "./pages/employee/EditTaskPage.jsx";
+import ViewTaskList from "./pages/employee/ViewTaskList.jsx";
+import GetAttendance from "./pages/employee/GetAttendance.jsx";
+import EmployeeList from "./pages/employee/EmployeeList.jsx";
+import EattendenceList from "./pages/employee/EattendenceList.jsx";
 
 // harvest
-<<<<<<< Updated upstream
-=======
 import HarvestDashboard from "./pages/Harvest/HarvestDashboard";
 import HarvestSchedule from"./pages/Harvest/HarvestSchedule";
-
-import InventoryDashboard from"./pages/Inventory/InventoryDashboard";
-import FertilizerForm from "./pages/Inventory/FertilizerForm.jsx";
-import Maintenance from "./pages/Inventory/FertilizerForm.jsx";
-import FertilizerForm from "./pages/Inventory/FertilizerForm.jsx";
->>>>>>> Stashed changes
-
+import YieldRecords from "./pages/Harvest/YieldRecords.jsx";
+import TaskAssign from "./pages/Harvest/TaskAssign.jsx"
+import AddHarvestSchedule from "./pages/Harvest/AddHarvestSchedule.jsx";
+import EditHarvestSchedule from './pages/Harvest/EditHarvestSchedule';
 // crop care
 
 // product
+import ProductsDashboard from "./pages/Products/ProductsDashboard.jsx";
+import ProductionScheduleOverview from "./pages/Products/ProductionScheduleOverview.jsx";
+import QualityControl from "./pages/Products/QualityControl.jsx";
+import AddSchedule from "./pages/Products/AddSchedule.jsx";
+import EditSchedule from "./pages/Products/EditSchedule.jsx";
+import AddInspectionReport from "./pages/Products/AddInspectionReport.jsx";
 
 // field view
 import CultivationDashboard from "./pages/CropVarieties/CultivationDashboard.jsx";
-
+import VarietyCrop from "./pages/CropVarieties/varietyCrop.jsx";
+import CropVarietyForm from "./pages/CropVarieties/CropVarietyForm.jsx";
+import Seedling from "./pages/CropVarieties/Seedling.jsx";
+import Schedules from "./pages/CropVarieties/Schedules.jsx";
+import ScheduleForm from "./pages/CropVarieties/ScheduleForm.jsx";
+import LandPreparation from "./pages/CropVarieties/LandPreparation.jsx";
 
 // buyers
+import BuyerRegistrationForm from "./pages/BuyerRegistrationForm.jsx";
+import BuyerTable from "./pages/BuyerTable.jsx";
+import Profile from './components/Profile';
 
 
 // import Test from "./pages/Test.jsx";
 import PageError from "./pages/PageError.jsx";
+
+
 export default function App() {
   return (
     <Routes>
@@ -49,36 +81,64 @@ export default function App() {
       <Route path="/dashboard" element={<Dashboard />} />
 
       {/* finance */}
+      <Route path="/salesAndFinance/" element={<SalesAndFinanceDashboard />} />
+      <Route path="/salesAndFinance/sales/" element={<SalesDashboard />} />
+      <Route path="/salesAndFinance/sales/addSalesRecord" element={<AddSalesRecordDashboard />} />
+      <Route path="/salesAndFinance/sales/viewSalesRecord" element={<ViewSalesRecordDashboard />} />
+      <Route path="/salesAndFinance/sales/analytics" element={<SalesAnalyticsPage />} />
+      <Route path="/salesAndFinance/finance/" element={<FinanceDashboard />} />
+      <Route path="/salesAndFinance/finance/add-transaction" element={<AddTransactionPage />} />
+      <Route path="/salesAndFinance/finance/transaction-display" element={<TransactionDisplay />} />
+      <Route path="/salesAndFinance/finance/analytics" element={<FinancialAnalyticsPage />} />
 
       {/* inventory */}
-<<<<<<< Updated upstream
-      <Route path="/Inventory/InventoryDashboard" element={<InventoryDashboard/>}/>
-      <Route path="/Inventory/Fertilizer" element={<Fertilizer/>}/>
-      <Route path="/Inventory/FertilizerForm" element={<FertilizerForm />} />
-      <Route path="/Inventory/Maintenance" element={<Maintenance/>}/>
-      <Route path="/Inventory/MaintenanceForm" element={<MaintenanceForm />} />
-      
-=======
-      <Route path="/Inventory/InventoryDashboard" element={<InventoryDashboard />} />
-      <Route path="/Inventory/Fertilizer" element={<Fertilizer />} />
-      <Route path="/Inventory/FertilizerForm" element={<FertilizerForm />} />
-      <Route path="/Inventory/Maintenance" element={<Maintenance />} />
-      <Route path="/Inventory/MaintenanceForm" element={<MaintenanceForm />} />
-      
+      <Route path="/Inventory/Fertilizer" element={<Fertilizer/>} />
+      <Route path="/Inventory/Maintenance" element={<Maintenance/>} />
+      <Route path="/Inventory/InventoryDashboard" element={<InventoryDashboard/>} />
+      <Route path="/Inventory/FertilizerForm" element={<FertilizerForm/>} />
+      <Route path="/Inventory/MaintenanceForm" element={<MaintenanceForm/>} />
 
->>>>>>> Stashed changes
-      {/* employee */}
+      {/* employee */}        
+      <Route path="/employee/dashboard" element={<Edashboard/>}/>
+      <Route path="/employee/registration" element= {<Eregistration/>}/>
+      <Route path="/employee/salary" element= {<Esalary/>}/>
+      <Route path="/employee/task" element= {<EaddTask/>}/>
+      <Route path="/employee/taskedit" element= {<EditTaskPage/>}/>
+      <Route path="/employee/TaskListview" element= {<ViewTaskList/>}/>
+      <Route path="/employee/attendance" element= {<GetAttendance/>}/>
+      <Route path="/employee/employeelist" element= {<EmployeeList/>}/>
+      <Route path="/employee/attendanceList" element= {<EattendenceList/>}/>
 
       {/* harvest */}
-
+      <Route path="/harvest/harvestdashboard" element={<HarvestDashboard />} />
+      <Route path="/harvest/harvest-schedule" element={<HarvestSchedule />} />
+      <Route path="/harvest/yield" element={<YieldRecords />} />
+      <Route path="/harvest/task" element={<TaskAssign/>} />
+      <Route path="/harvest/addschedule" element={<AddHarvestSchedule />} />
+      <Route path="/harvest/edit/:id" element={<EditHarvestSchedule />} />
       {/* crop care */}
 
       {/* product */}
+      <Route path="/products/productdashboard" element={<ProductsDashboard />} />
+      <Route path="/products/production-overview" element={<ProductionScheduleOverview />} />
+      <Route path="/products/quality-control" element={<QualityControl />} />
+      <Route path="/products/addschedule" element={<AddSchedule />} />
+      <Route path="/products/editschedule/:id" element={<EditSchedule />} />
+      <Route path="/products/addInspectionReport" element={<AddInspectionReport />} />
 
       {/* field view  */}
-      <Route path="cultivationDashboard" element={<CultivationDashboard />} />
-
+      <Route path="/cultivationDashboard" element={<CultivationDashboard />} />
+      <Route path="/varietyCrop" element={<VarietyCrop />} />
+      <Route path="/cvForm" element={<CropVarietyForm />} />
+      <Route path="/seedlingDistribution" element={<Seedling />} />
+      <Route path="/schedules" element={<Schedules />} />
+      <Route path="/scheduleForm" element={<ScheduleForm />} />
+      <Route path="/landPreparation" element={<LandPreparation />} />
+        
       {/* buyers */}
+      <Route path="/buyer-registration" element={<BuyerRegistrationForm />} />
+      <Route path="/buyert" element={<BuyerTable />} />
+      <Route path="/profile" element={<Profile />} />
 
       {/* page not found & error page */}
       {/* <Route path="/test" element={<Test />} /> */}
