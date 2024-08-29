3c81c45bdba80702c3e614f99457bb6c78493ae4
maheesha999 <maheeshaliyanage999@gmail.com>
2024-08-29 09:47:18 +0530

    update inventorybackend files (#99)

---
+++ backend/routes/Inventory/fertilizerRoute.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const {
+    createFertilizers,
+    getAllFertilizers,
+    getFertilizersById,
+    updateFertilizers,
+    deleteFertilizers,
+} = require('../../controllers/Inventory/fertilizerController'); // Adjusted path
+
+
+const router = express.Router();
+
+router.post('/', createFertilizers);
+router.get('/', getAllFertilizers);
+router.get('/:id', getFertilizersById);
+router.put('/:id', updateFertilizers);
+router.delete('/:id', deleteFertilizers);
+
+module.exports = router;
+
+
