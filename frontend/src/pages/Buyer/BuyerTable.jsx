aa7e6f1ffb388edbcbef62a25b25537d277b1208
UVINDUSEN <uvisends24946@gmail.com>
2024-09-02 15:29:12 +0530

    uvi
    m

---
+++ frontend/src/pages/Buyer/BuyerTable.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import Header from '../components/Header';
+// // import Sidebar from '../components/Sidebar';
+// import UserManagementTable from '../components/UserManagementTable';
+
+// function BuyerTable() {
+//   return (
+//     <div>
+//      <Header />
+//       {/* <Sidebar style={{ width: '250px' }} /> */}
+    
+//       <UserManagementTable />
+//     </div>
+//   );
+// }
+
+// export default BuyerTable;
+
