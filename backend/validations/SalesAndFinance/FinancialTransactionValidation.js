8a449c7514b7d89481ef38148b2fd3a5c7793b92
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-09-03 12:33:13 +0530

    Update FinancialTransactionValidation.js

--- backend/validations/SalesAndFinance/FinancialTransactionValidation.js
+++ backend/validations/SalesAndFinance/FinancialTransactionValidation.js
@@ -6,16 +6,17 @@ const financialTransactionSchema = Joi.object({
   transactionType: Joi.string().valid('Credit', 'Debit').required(),
   amount: Joi.number().positive().required(),
   date: Joi.date().optional(),
-  associatedEntity: Joi.string()
-    .custom((value, helpers) => {
-      if (!mongoose.Types.ObjectId.isValid(value)) {
-        return helpers.error('any.invalid');
-      }
-      return value;
-    })
-    .required(),
+  // associatedEntity: Joi.string()
+  //   .custom((value, helpers) => {
+  //     if (!mongoose.Types.ObjectId.isValid(value)) {
+  //       return helpers.error('any.invalid');
+  //     }
+  //     return value;
+  //   })
+  //   .required(),
+  transactionCategory: Joi.string().valid('Income', 'Expense').required(),
   entityModel: Joi.string().valid('Buyer', 'Supplier', 'Employee', 'Other').required(),
-  description: Joi.string().max(500).optional(),
+  payeePayer: Joi.string().max(500).optional(),
 });
 
 module.exports = { financialTransactionSchema };
