daa640790999d51d1531f695ec616f65840a1732
UVINDUSEN <uvisends24946@gmail.com>
2024-09-17 23:40:53 +0530

    m

--- backend/routes/buyerRoute.js
+++ backend/routes/buyerRoute.js
@@ -1,23 +1,25 @@
-// routes/buyerRoute.js
-const express = require('express');
-const router = express.Router();
-const { registerBuyer } = require('../controllers/buyerController');
-
-// Route to register a new buyer
-router.post('/register', registerBuyer);
-
-module.exports = router;
-backend/routes/buyerRoute.js
-
+// // routes/buyerRoute.js
 // const express = require('express');
-// const { createBuyer, getAllBuyers, getBuyerById, updateBuyerById, deleteBuyerById } = require('../controllers/buyerController');
-
 // const router = express.Router();
+// const  registerBuyer  = require('../controllers/buyerController');
 
-// router.post('/', createBuyer);
-// router.get('/', getAllBuyers);
-// router.get('/:id', getBuyerById);
-// router.put('/:id', updateBuyerById);
-// router.delete('/:id', deleteBuyerById);
+// // Route to register a new buyer
+// router.post('/register', registerBuyer);
 
 // module.exports = router;
+// backend/routes/buyerRoute.js
+
+
+
+const express = require('express');
+const { createBuyer, getAllBuyers, getBuyerById, updateBuyerById, deleteBuyerById } = require('../controllers/buyerController');
+
+const router = express.Router();
+
+router.post('/', createBuyer);
+router.get('/', getAllBuyers);
+router.get('/:id', getBuyerById);
+router.put('/:id', updateBuyerById);
+router.delete('/:id', deleteBuyerById);
+
+module.exports = router;
