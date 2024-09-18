9a61daf993931a63feb0631c0a1037accd3cd734
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-09-18 06:44:38 +0530

    Update EaddTask.jsx

--- frontend/src/pages/employee/EaddTask.jsx
+++ frontend/src/pages/employee/EaddTask.jsx
@@ -5,34 +5,25 @@ import ETaskForm from "../../components/Employee/ETaskForm";
 import EmployeeNavbar from "../../components/Employee/EmployeeNavbar";
 import { Breadcrumb } from "antd";
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import Breadcrumbs from "../../components/Employee/Breadcrumbss";
 
-export default function EAddTask(){
-    const breadcrumbItems = [
-        { name: 'Employees', href: '/employees/home' },
-        { name: 'Assign Tasks', href: '/employees/tasks' },
-        { name: 'Add new task', href: '/employees/tasks/addTask' },
-    ];
-    return (
-        <div className="flex-col">
-           
-            
-                <Header />
-                <Sidebar />
-                
-                        <div className={`ml-[300px]`}>
-                        <Breadcrumb
-          items={[
-            {
-              href: "",
-              title: <HomeOutlined />,
-            },
-          ]}
-        />
-                       <EmployeeNavbar/>
-                            <ETaskForm/>
-                        </div>
-                    </div>
+export default function EAddTask() {
+  const breadcrumbItems = [
+    { name: "Employees", href: "/employees/home" },
+    { name: "Assign Tasks", href: "/employees/tasks" },
+    { name: "Add new task", href: "/employees/tasks/addTask" },
+  ];
+  return (
+    <div className="flex-col">
+      <Header />
+      <Sidebar />
 
-                  
-    );
-};
+      <div className={`ml-[300px]`}>
+       
+        <EmployeeNavbar />
+        <Breadcrumbs items={breadcrumbItems} />
+        <ETaskForm />
+      </div>
+    </div>
+  );
+}
