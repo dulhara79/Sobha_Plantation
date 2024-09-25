6c2fd84c0cdbdc44fe00479fb49df6b7c8f90077
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-09-25 22:35:24 +0530

    Create ValuationDashboard.jsx

---
+++ frontend/src/pages/SalesAndFinance/Finance/ValuationDashboard.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+import Sidebar from "../../../components/Sidebar";
+import Header from "../../../components/Header";
+import NavigationButtons from "../../../components/Sales_and_Finance/NavigationButtons";
+
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { Breadcrumb } from "antd";
+
+import {useNavigate} from "react-router-dom";
+import { useSnackbar } from 'notistack';
+
+import Valuation from '../../../components/Sales_and_Finance/Finance/Valuation/Valuation';
+
+import NewLoadingScreen from '../../../components/LoadingDots'
+
+const ValuationDashboard = () => {
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      // Simulate loading process (e.g., API calls, component mounting)
+      setTimeout(() => {
+        setLoading(false); // Once the components or data are loaded
+      }, 2000); // Adjust the delay as needed
+    }, []);
+  
+    if (loading) return <NewLoadingScreen />;
+  return (
+    <div>
+    <Header />
+    <Sidebar activePage="/salesAndFinance/"/>
+      <div className={`ml-[300px] pt-3 w-full`}>
+    <Breadcrumb
+    style={{ margin: "10px 0" }}
+        items={[
+          {
+            href: "/dashboard",
+            title: <HomeOutlined />,
+          },
+          {
+            href: "/salesAndFinance/sales/",
+            title: (
+              <>
+                <UserOutlined />
+                <span>Sales and Finance</span>
+              </>
+            ),
+          },{
+            href: "/salesAndFinance/sales/",
+            title: (
+              <>
+                <span>Sales</span>
+              </>
+            ),
+          },
+          { 
+            title: "Sales Dahsboard",
+          },
+        ]}
+      />
+      <NavigationButtons />
+      </div>
+      <div className={`ml-[300px] pt-3 w-[100px]`}>
+      <Valuation/>
+      </div>
+    </div>
+  )
+}
+
+export default ValuationDashboard
