11991ffb0f2404905e03e36659dcacd326bf61b8
UVINDUSEN <uvisends24946@gmail.com>
2024-09-25 18:42:35 +0530

    Create buyerInfoRoute.js

---
+++ backend/routes/buyerInfoRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getAllBuyerInfoRecords,
+    createBuyerInfoRecords,
+    getBuyerInfoRecordsById,
+    updateBuyerInfoRecords,
+    deleteBuyerInfoRecords,
+    } = require("../controllers/buyerInfoController"); 
+
+// Routes for Info Management
+
+router.get("/", getAllBuyerInfoRecords);
+router.post("/", createBuyerInfoRecords);
+router.get("/:InfoId", getBuyerInfoRecordsById);
+router.put("/:InfoId", updateBuyerInfoRecords);
+router.delete("/:InfoId", deleteBuyerInfoRecords);
+
+module.exports = router;
