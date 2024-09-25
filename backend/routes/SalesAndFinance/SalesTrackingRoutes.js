d56a72c0d1dc11a58e5a659d44bb06539579c49b
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-09-25 19:12:02 +0530

    Update SalesTrackingRoutes.js

--- backend/routes/SalesAndFinance/SalesTrackingRoutes.js
+++ backend/routes/SalesAndFinance/SalesTrackingRoutes.js
@@ -5,21 +5,29 @@ const {
   getSalesTrackingById,
   updateSalesTracking,
   deleteSalesTracking,
-} = require("../../controllers/SalesAndFinance/salesTrackingController");
+  getWeeklySalesSummary
+} = require("../../controllers/SalesAndFinance/SalesTrackingController");
 const { validate } = require("../../middleware/SalesAndFinance/validateMiddleware");
 const { salesTrackingSchema } = require("../../validations/SalesAndFinance/salesTrackingValidation");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(getAllSalesTracking)
-  .post(validate(salesTrackingSchema), createSalesTracking);
+router.post("/", createSalesTracking);
+router.get("/", getAllSalesTracking);
+// router.get("/:id", getSalesTrackingById);
+router.put("/:id", updateSalesTracking);
+router.delete("/:id", deleteSalesTracking);
+router.get('/weekly-summary', getWeeklySalesSummary);
 
-router
-  .route("/:id")
-  .get(getSalesTrackingById)
-  .put(validate(salesTrackingSchema), updateSalesTracking)
-  .delete(deleteSalesTracking);
+// router
+//   .route("/")
+//   .get(getAllSalesTracking)
+//   .post(validate(salesTrackingSchema), createSalesTracking);
+
+// router
+//   .route("/:id")
+//   .get(getSalesTrackingById)
+//   .put(validate(salesTrackingSchema), updateSalesTracking)
+//   .delete(deleteSalesTracking);
 
 module.exports = router;
