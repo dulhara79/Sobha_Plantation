cc40642673b71dcf7a5ad247aaadfeb69e871a48
Dulhara Kaushalya <128912391+dulhara79@users.noreply.github.com>
2024-09-25 20:43:11 +0530

    Develop/dulhara (#135)
    * Create SalaryEmployeeDetails.js
    
    * Update SalesAnalyticsController.js
    
    * Update SalesTrackingController.js
    
    * Update ValuationController.js
    
    * Create SalaryModel.js
    
    * Delete NewLoadingScreen.jsx
    
    * Delete LoadingScreen.jsx
    
    * Create loadingScreen.gif
    
    * Update LoadingDots.jsx
    
    * Update UpdateTransactionPage.jsx
    
    * Update SalesTrackingModel.js
    
    * Create SalaruRoute.js
    
    * Update SalesAnalyticsRoutes.js
    
    * Update SalesTrackingRoutes.js
    
    * Delete EmployeeSalary.jsx
    
    * Update AddTransaction.jsx
    
    * Create EmployeeSalary.jsx

--- backend/models/SalesAndFinance/SalesTrackingModel.js
+++ backend/models/SalesAndFinance/SalesTrackingModel.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 
+// Define the schema
 const salesTrackingSchema = new mongoose.Schema(
   {
     product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
+      type: String, // Use product name as string
       required: [true, 'Product is required'],
+    },
+    saleDate: {
+      type: Date,
+      default: Date.now,
     },
     quantitySold: {
       type: Number,
       required: [true, 'Quantity sold is required'],
       min: [1, 'Quantity sold cannot be less than 1'],
     },
-    saleDate: {
-      type: Date,
-      default: Date.now,
+    unitPrice: {
+      type: Number,
+      required: [true, 'Unit price is required'],
     },
     revenueGenerated: {
       type: Number,
@@ -25,4 +29,5 @@ const salesTrackingSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Export the model
 module.exports = mongoose.model('SalesTracking', salesTrackingSchema);
