3be6207bfb4daf21022c1b93fdee070353433de7
UVINDUSEN <uvisends24946@gmail.com>
2024-09-28 09:55:45 +0530

    Update UpdateInfoTable.jsx

--- frontend/src/pages/Buyer/UpdateInfoTable.jsx
+++ frontend/src/pages/Buyer/UpdateInfoTable.jsx
@@ -3,9 +3,12 @@ import React, { useState, useEffect } from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import { HomeOutlined, LeftOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button, Input, Form, notification } from "antd";
+import { Breadcrumb, Button, Input, Form, notification, DatePicker, Select  } from "antd";
 import { Link, useNavigate, useParams } from "react-router-dom"; // Import useParams
 import axios from "axios";
+import moment from 'moment';
+
+const { Option } = Select;
 
 const UpdateBuyerInfoRecords = () => {
   const [form] = Form.useForm();
@@ -53,7 +56,7 @@ const UpdateBuyerInfoRecords = () => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await axios.get(`http://localhost:8090/api/InfoRecords/${id}`);
+        const response = await axios.get(`http://localhost:8090/api/buyerInfo/${id}`);
         const data = response.data.BuyerInfoRecord
         console.log("response.data: " + response.data.InfoRecord);
         console.log(response.data.BuyerInfoRecord);
@@ -92,7 +95,7 @@ const UpdateBuyerInfoRecords = () => {
       setLoading(true);
 
       // Make a PUT request to update the record
-      await axios.put(`http://localhost:8090/api/InfoRecords/${id}`, values);
+      await axios.put(`http://localhost:8090/api/buyerInfo/${id}`, values);
 
       notification.success({
         message: "Record Updated",
@@ -114,6 +117,9 @@ const UpdateBuyerInfoRecords = () => {
 
   const handleCancel = () => {
     navigate("/BInfotable");
+  };
+  const disabledDate = (current) => {
+    return current && current > moment().endOf("day");
   };
 
   return (
@@ -157,92 +163,64 @@ const UpdateBuyerInfoRecords = () => {
               className="mt-6"
               onFinish={handleSubmit}
             >
-              <Form.Item
-                label="First Name"
-                name="firstName"
-                rules={alphabeticRule}
-              >
+              <Form.Item label="First Name" name="firstName" rules={alphabeticRule}>
                 <Input placeholder="Enter first name" />
               </Form.Item>
 
-              <Form.Item
-                label="Last Name"
-                name="lastName"
-                rules={alphabeticRule}
-              >
+              <Form.Item label="Last Name" name="lastName" rules={alphabeticRule}>
                 <Input placeholder="Enter last name" />
               </Form.Item>
 
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[
-                  {
-                    type: "email",
-                    message: "Please enter a valid email address",
-                  },
-                  {
-                    required: true,
-                    message: "Please enter your email",
-                  },
-                ]}
-              >
-                <Input placeholder="Enter email" />
+              <Form.Item label="User Name" name="userName" rules={alphabeticRule}>
+                <Input placeholder="Enter User Name" />
               </Form.Item>
 
-              <Form.Item
-                label="Address"
-                name="address"
-                rules={alphabeticRule}
-              >
-                <Input placeholder="Enter address" />
+              <Form.Item label="Password" name="Password" rules={alphabeticNumericRule}>
+                <Input type="password" placeholder="Enter a Password" />
               </Form.Item>
 
-              <Form.Item
-                label="City"
-                name="city"
-                rules={alphabeticNumericRule}
-              >
-                <Input placeholder="Enter your city" />
+              <Form.Item label="Confirm Password" name="ConfirmPassword" rules={alphabeticNumericRule}>
+                <Input type="password" placeholder="Confirm your Password" />
               </Form.Item>
 
-              <Form.Item
-                label="Country"
-                name="country"
-                rules={alphabeticNumericRule}
-              >
-                <Input placeholder="Enter your country" />
+              <Form.Item label="Gender" name="Gender" rules={[{ required: true, message: "Please select your gender" }]}>
+                <Select placeholder="Select your Gender">
+                  <Option value="male">Male</Option>
+                  <Option value="female">Female</Option>
+                  <Option value="other">Prefer not to say</Option>
+                </Select>
               </Form.Item>
 
-              <Form.Item
-                label="Postal Code"
-                name="postalCode"
-                rules={numericRule}
-              >
-                <Input placeholder="Enter your postal code" />
+              <Form.Item label="Date of Birth" name="DOB" rules={[{ required: true, message: "Please select your date of birth" }]}>
+              <DatePicker format="YYYY-MM-DD" disabledDate={disabledDate} disabled/>
               </Form.Item>
 
-              <Form.Item
-                label="Phone"
-                name="phone"
-                rules={numericRule}
-              >
+              <Form.Item label="Number" name="Number" rules={numericRule}>
                 <Input placeholder="Enter your phone number" />
               </Form.Item>
 
-              <div className="flex justify-center mt-4 space-x-4">
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  loading={loading}
-                  style={{ backgroundColor: "#236A64", color: "#fff" }}
-                >
+              <Form.Item label="Email" name="email" rules={[
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!",
+                  },
+                  {
+                    required: true,
+                    message: "Please input your E-mail!",
+                  },
+                ]}
+              >
+                <Input placeholder="Enter your email" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit" loading={loading}>
                   Submit
                 </Button>
-                <Button type="default" htmlType="button" onClick={handleCancel}>
+                <Button type="default" onClick={handleCancel} className="ml-2">
                   Cancel
                 </Button>
-              </div>
+              </Form.Item>
             </Form>
           </div>
         </div>
