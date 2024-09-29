3a5e38acf96b18dd3c1de65b7565208d9d4d2c8e
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2024-09-29 18:36:23 +0530

    Update EaddTask.jsx

--- frontend/src/pages/employee/EaddTask.jsx
+++ frontend/src/pages/employee/EaddTask.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import ETaskForm from "../../components/Employee/ETaskForm";
@@ -6,8 +6,21 @@ import EmployeeNavbar from "../../components/Employee/EmployeeNavbar";
 import { Breadcrumb } from "antd";
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
 import Breadcrumbs from "../../components/Employee/Breadcrumbss";
+import NewLoadingScreen from "../../components/LoadingDots";
 
-export default function EAddTask() {
+
+
+  const EAddTask = () => {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      // Simulate loading process (e.g., API calls, component mounting)
+      setTimeout(() => {
+        setLoading(false); // Once the components or data are loaded
+      }, 2000); // Adjust the delay as needed
+    }, []);
+  
+    if (loading) return <NewLoadingScreen />;
+    
   const breadcrumbItems = [
     { name: "Employees", href: "/employees/home" },
     { name: "Assign Tasks", href: "/employees/tasks" },
@@ -26,4 +39,6 @@ export default function EAddTask() {
       </div>
     </div>
   );
-}
+};
+
+export default EAddTask;
