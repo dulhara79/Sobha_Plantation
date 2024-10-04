1bd9bec293ae769c36d4f3d495fc598b43963142
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2024-10-04 16:23:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -200,7 +200,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
