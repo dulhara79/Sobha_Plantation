f95007520525a67bd1a3b0c517ade159b7f7fb6d
PiyushiRan <piyushiranasinghe@gmail.com>
2024-10-13 12:52:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -154,9 +154,9 @@ import PlantGrowth from "./pages/CropVarieties/plantGrowth.jsx";
 
 // buyers
 // import Cart from "./pages/Buyer/Cart.jsx";
-impor tBuyerRegistrationForm from './pages/BuyerRegistrationForm.jsx';
-import BuyerTable from "./pages/BuyerTable.jsx";
-import Profile from './components/Profile';
+//import BuyerRegistrationForm from './pages/BuyerRegistrationForm.jsx';
+/*import BuyerTable from "./pages/BuyerTable.jsx";
+//import Profile from './components/Profile';
 import BuyerDashboard from './pages/Buyer/BuyerDashboard.jsx';
 
 import BuyerDelivery from "./pages/Buyer/BuyerDelivery.jsx";
@@ -169,7 +169,7 @@ import UpdateBuyerInfo from "./pages/Buyer/UpdateInfoTable.jsx";
 
 // import BuyerPreOrderForm from './pages/Buyer/BuyerPreOrderForm';
 // import BuyerPreOrderTable from './pages/Buyer/BuyerPreOrderTable';
-// import UpdateBuyerPreOrderRequests from './pages/Buyer/UpdatePreOrderForm';
+// import UpdateBuyerPreOrderRequests from './pages/Buyer/UpdatePreOrderForm';*/
 
 import PageError from "./pages/PageError.jsx";
 
@@ -412,24 +412,24 @@ export default function App() {
       <Route path="/pGrowth" element={<PlantGrowth />} />
 
       {/* buyers */}
-      <Route path="/register-buyer" element={<BuyerRegistrationForm />} />
+      {/*<Route path="/register-buyer" element={<BuyerRegistrationForm />} />
       <Route path="/buyert" element={<BuyerTable />} />
 
 
-      <Route path="/profile" element={<Profile />} />
+     // <Route path="/profile" element={<Profile />} />
       <Route path="/Bdelivery" element={<BuyerDelivery />} />
       <Route path="/Bdeliverytable" element={<BuyerDeliveryTable />} />
       <Route path="/updateDelivery/:id" element={<UpdateDeliveryTable />} />
 
       <Route path="/buyerinfo" element={<BuyerInfo />} />
       <Route path="/buyerinfotable" element={<BuyerInfoTable />} />
-      <Route path="/updateBuyer/:id" element={<UpdateBuyerInfo />} /> 
+      <Route path="/updateBuyer/:id" element={<UpdateBuyerInfo />} /> */}
 
        {/* <Route path="/create-preorder" element={<BuyerPreOrderForm/> } />
       <Route path="/preorders" element={<BuyerPreOrderTable/>} />
       <Route path="/update-preorder/:id" element={<UpdateBuyerPreOrderRequests/>} />  */}
 
-      <Route path="/buyerdashboard" element={<BuyerDashboard />} />
+      {/*<Route path="/buyerdashboard" element={<BuyerDashboard />} />*/}
 
       {/* page not found & error page */}
       {/* <Route path="/test" element={<Test />} /> */}
