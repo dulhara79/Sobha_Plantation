16ee58aa8e0dd092da9b565b4d12cf41d5f4b399
Dewmini Nawodya <161587704+dewmininawodya26@users.noreply.github.com>
2024-10-13 07:25:41 +0530

    Develop/dewmini1 (#221)
    * Create BarGraph.jsx
    
    * Update AddPackage.jsx
    
    * Update EditPackage.jsx
    
    * Update Labeling.jsx
    
    * Update Packaging.jsx
    
    * Update ProductsDashboard.jsx

---
+++ frontend/src/components/Products/Labeling/BarGraph.jsx
@@ -0,0 +1,27 @@
+// BarGraph.jsx
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Legend } from 'recharts';
+
+const BarGraph = ({ data }) => {
+  return (
+    <BarChart
+      width={900} // Adjust the width as needed
+      height={600} // Adjust the height as needed
+      data={data}
+      margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar 
+        dataKey="value" 
+        fill="#20C997" // Change color as needed
+        animationDuration={500} // Animation duration in milliseconds
+      />
+    </BarChart>
+  );
+};
+
+export default BarGraph;
