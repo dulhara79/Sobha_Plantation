fbd1418130345569c380135799a3ade87eb9ff8f
PiyushiRan <piyushiranasinghe@gmail.com>
2024-10-14 17:13:25 +0530

    Update Schedules.jsx

--- frontend/src/pages/CropVarieties/Schedules.jsx
+++ frontend/src/pages/CropVarieties/Schedules.jsx
@@ -376,16 +376,16 @@ const VarietySchedule = () => {
             </div>
            {/* Button container for alignment */}
            <div style={{ display: 'flex', gap: '8px' }}>
-              <Button type="primary" onClick={() => navigate('/scheduleForm')}>
+              <button type="primary" className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600" onClick={() => navigate('/scheduleForm')}>
                 Add Schedule
-              </Button>
-              <Button
+              </button>
+              <button
                 type="primary"
                 onClick={generateReport}
                 className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
               >
                 Generate Report
-              </Button>
+              </button>
           </div>
           </div>
 
