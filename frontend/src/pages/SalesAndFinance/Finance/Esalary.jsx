d225912d050cfb521295eca8e277080bf5477231
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-10-14 13:02:46 +0530

    Update Esalary.jsx

--- frontend/src/pages/SalesAndFinance/Finance/Esalary.jsx
+++ frontend/src/pages/SalesAndFinance/Finance/Esalary.jsx
@@ -16,7 +16,7 @@ const Esalary = () => {
       // Simulate loading process (e.g., API calls, component mounting)
       setTimeout(() => {
         setLoading(false); // Once the components or data are loaded
-      }, 2000); // Adjust the delay as needed
+      }, 1000); // Adjust the delay as needed
     }, []);
   
     if (loading) return <NewLoadingScreen />;
