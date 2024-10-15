985bd31ebd0f2a98a5eca458abb3d6e8656c7e23
Dewmini Nawodya <dewmininawodya26@gmail.com>
2024-10-15 10:00:17 +0530

    Update HarvestSchedule.jsx

--- frontend/src/pages/Harvest/HarvestSchedule.jsx
+++ frontend/src/pages/Harvest/HarvestSchedule.jsx
@@ -97,6 +97,7 @@ const HarvestSchedule = () => {
 
   const generatePDF = async () => {
     const doc = new jsPDF();
+    const today = moment().format("YYYY-MM-DD");
 
     // Load the logo image
     const logoUrl = "../src/assets/logo.png";
@@ -121,12 +122,13 @@ const HarvestSchedule = () => {
       doc.text("Kurunagala, Sri Lanka.", 10, 20); // Address line 2
       doc.text("Email: sobhaplantationsltd@gmail.com", 10, 25); // Email address line
       doc.text("Contact: 0112 751 757", 10, 30); // Email address line
+      doc.text(`Date: ${today}`, 10, 35);
        // Header with logo
        if (logoDataURL) {
         doc.addImage(logoDataURL, 'PNG', pageWidth - 50, 10, 40, 10); // Align right (adjust the x position as needed)
       }
   
-      doc.line(10, 35, pageWidth - 10, 35); // Header line
+      doc.line(10, 38, pageWidth - 10, 38); // Header line
   
       // Footer with page number
       doc.setFontSize(10);
